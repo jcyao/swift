fb31700b79159bd03e0f36bf5efb26534c606e79
yaojuncheng3 <yaojuncheng3@jd.com>
2024-07-23 14:57:36 +0800

    vad back to v4

--- next.config.mjs
+++ next.config.mjs
@@ -44,7 +44,7 @@ async function copyFiles() {
 			"public/vad.worklet.bundle.min.js"
 		),
 		fs.copyFile(
-			"silero_vad.onnx",
+			"node_modules/@ricky0123/vad-web/dist/silero_vad.onnx",
 			"public/silero_vad.onnx"
 		),
 		...wasmFiles.map((file) =>
