cc30ee4be50cac76e161e815b339c191473187fc
yaojuncheng3 <junchengyao@outlook.com>
2024-08-08 09:48:53 +0800

    update sr to 8k

--- app/api/route.ts
+++ app/api/route.ts
@@ -88,7 +88,7 @@ export async function POST(request: Request) {
 			output_format: {
 				container: "raw",
 				encoding: "pcm_s16le",
-				sample_rate: 16000,
+				sample_rate: 8000,
 			},
 		}),
 	});
